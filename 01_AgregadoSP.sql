de280edb57849b37e370422f69d2916cf1f71373
Agustinp96 <agustinpe45@gmail.com>
2025-11-11 17:30:11 -0300

    Update 01_AgregadoSP.sql

--- 01_AgregadoSP.sql
+++ 01_AgregadoSP.sql
@@ -9,7 +9,6 @@
 -- Vazquez, Isaac Benjamin                     --
 -- Pizarro Dorgan, Fabricio Alejandro          --
 -- Piñero, Agustín                             --
--- Nardelli Rosales, Cecilia Anahi             --
 -- Comerci Salcedo, Francisco Ivan             --
 -------------------------------------------------
 -------------------------------------------------
@@ -453,3 +452,4 @@ BEGIN
     END CATCH
 END
 GO
+
